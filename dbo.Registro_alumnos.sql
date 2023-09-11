62b57e27e24b6d005b7add18be7a270c4b2ab565
CamilaOlivi <144635467+CamilaOlivi@users.noreply.github.com>
2023-09-10 21:54:21 -0300

    added database

---
+++ dbo.Registro_alumnos.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Registro_alumnos] (
+    [Id]              INT           NOT NULL,
+    [Rut_Alumno]      INT           NULL,
+    [Nombre_Alumno]   NVARCHAR (30) NULL,
+    [Apellido_Alumno] NVARCHAR (30) NULL,
+    [Fecha_Ingreso]   DATE          NULL,
+    [Edad]            INT           NULL,
+    [Telefono]        NUMERIC (9)   NULL,
+    [Direccion]       NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
